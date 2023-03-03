389e3331e77ffaba207b1d6ae0fed4a047585331
Julian Henao Henao <jhenaohenao@gmail.com>
2023-03-02 19:38:41 -0500

    Añade menú desplegable al sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import { HomeComponent } from './home/home.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 // Fin modulos Material
 @NgModule({
   declarations: [
@@ -30,7 +33,10 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    MatCardModule
+    MatCardModule,
+    MatExpansionModule,
+    MatListModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
